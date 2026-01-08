dd88e0ae66db6d569342614675f131fd6ea37c3a
Fox <filipiukjarek@gmail.com>
2026-01-08 15:48:14 +0100

    Command line interface implementation/refactor.

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,51 @@
-use clap::Parser;
+use clap::{CommandFactory, Parser};
 use code_cnt::config_reader::{Config, ConfigError};
 use code_cnt::registry::LangRegistry;
 use std::path::PathBuf;
 
 #[derive(Parser)]
 #[command(author, version, about)]
-struct Args {
-    dir: PathBuf,
+struct Cli {
+    /// Directory where the files are. Runs with default configuration.
+    #[arg(short, long)]
+    dir: Option<PathBuf>,
+
+    /// Config path
+    #[arg(short, long)]
+    cfg: Option<String>,
 }
 
 fn main() -> Result<(), ConfigError> {
-    let args = Args::parse();
+    let args = Cli::parse();
     let arg_dir = args.dir;
+    let arg_cfg = args.cfg;
 
-    println!("Processing directory: {}", arg_dir.display());
-    let config = Config::load("example.toml")?;
-
-    println!("MAIN: {}", config.dir.display());
-    println!("CONFIG\n {:#?}\n ******************\n\n", config);
-    //
-    //let mut lang_registry = LangRegistry::with_builtins_langs(&arg_dir);
-    //if let Err(e) = lang_registry.update_stats() {
-    //    eprintln!("Error updating stats: {e}");
-    //    std::process::exit(1);
-    //}
-
-    //lang_registry.show_stats();
-    let mut lang_registry_toml = LangRegistry::with_config(config)?; // config consumed here and
-                                                                     // will not be used after
-    if let Err(e) = lang_registry_toml.update_stats() {
-        eprintln!("Error updating stats: {e}");
-        std::process::exit(1);
+    if arg_cfg.is_none() && arg_dir.is_none() {
+        Cli::command().print_long_help()?;
+        return Ok(());
     }
-    lang_registry_toml.show_stats();
+
+    let arg_dir = match arg_dir {
+        Some(d) => d,
+        None => {
+            let arg_cfg = match arg_cfg {
+                Some(p) => p,
+                None => {
+                    Cli::command().print_long_help()?;
+                    return Ok(());
+                }
+            };
+            let config = Config::load(&arg_cfg)?;
+            println!("Config read successfully...");
+            let mut reg = LangRegistry::with_config(config)?;
+            reg.update_stats()?;
+            reg.show_stats();
+            return Ok(());
+        }
+    };
+    println!("No external configuration provided. Running with defaults...");
+    let mut reg = LangRegistry::with_builtins_langs(&arg_dir);
+    reg.update_stats()?;
+    reg.show_stats();
     Ok(())
 }
